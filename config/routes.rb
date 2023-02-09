960059917776a19048b3d8221db02b5b45e79dc5
EC2 Default User <ec2-user@ip-172-31-30-247.ap-northeast-1.compute.internal>
2023-02-09 08:56:21 +0000

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'  
+  get 'top' => 'home#top'
+  get 'todolists/new'
+  post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+
+  # ---- 下記1行を追加してください ---- #
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
